f3e69d241b0353a491d70e592f51842efa0988fa
MarcelloVendruscolo <marcello.pietro@hotmail.com>
2020-10-07 21:01:08 +0200

    Organised and structured the code. Included the final section of the assignment where the neural network model receives the test dataset as input and output predictions. Result is organised in predictions.csv file, as required by the assignment. Average of 97.5% accuracy.

--- imageAnalysis.py
+++ imageAnalysis.py
@@ -5,7 +5,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 dataset = pd.read_csv('trainset.csv')
-dataset_size = len(dataset)
+test_dataset = pd.read_csv('testset.csv')
 image_side = 28
 number_channels = 1
 
@@ -14,12 +14,6 @@ dataset_images = np.array(dataset.iloc[:,1:(image_side * image_side) + 1])
 dataset_images = dataset_images / 255
 
 dataset_images= np.array([img.reshape(image_side, image_side, number_channels) for img in dataset_images])
-
-learn_labels = dataset_labels[0:int(0.8*dataset_size)]
-validation_labels = dataset_labels[int(0.8*dataset_size):dataset_size]
-
-learn_images = dataset_images[0:int(0.8*dataset_size)]
-validation_images = dataset_images[int(0.8*dataset_size):dataset_size]
 
 model = models.Sequential()
 model.add(layers.Conv2D(32, (3, 3), activation="relu", input_shape=(image_side, image_side, number_channels)))
@@ -34,9 +28,22 @@ model.add(layers.Dense(10, activation="softmax"))
 model.summary()
 
 model.compile(optimizer='adam', loss=tf.keras.losses.SparseCategoricalCrossentropy(), metrics=['accuracy'])
-history = model.fit(learn_images, learn_labels, epochs=8, validation_data=(validation_images, validation_labels))
+history = model.fit(dataset_images, dataset_labels, epochs=6, validation_split=0.2)
 
-plt.plot(history.history['accuracy'], label='accuracy')
-plt.plot(history.history['val_accuracy'], label = 'val_accuracy')
+test_dataset_images = np.array(test_dataset.iloc[:,0:(image_side * image_side)])
+test_dataset_images = test_dataset_images / 255
+
+test_dataset_images= np.array([img.reshape(image_side, image_side, number_channels) for img in test_dataset_images])
+
+predictions = model(test_dataset_images)
+predictions = np.array(predictions)
+
+output_data = {'ImageId': np.arange(1, len(predictions) + 1, 1), 'Label': [probabilities.argmax() for probabilities in predictions]}
+output_data = pd.DataFrame(data = output_data)
+output_data.to_csv('predictions.csv', index = False)
+
+plt.title('Accuracy')
+plt.plot(history.history['accuracy'], label='learn')
+plt.plot(history.history['val_accuracy'], label = 'validation')
 plt.legend()
 plt.show()
